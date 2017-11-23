5deb3746b766ef0bdbbaa388dc9a9b1d62c5d163
Steven Casagrande <scasagrande@galvant.ca>
2017-11-23 12:59:33 -0500

    Test for bad log levels and return codes

--- tests/test_logserv.py
+++ tests/test_logserv.py
@@ -30,6 +30,7 @@ def test_empty_db(client):
     """Start with a blank database."""
     rv = client.get('/messages')
     assert b'[]' in rv.data
+    assert rv.status_code == 200
 
 
 @given(st.text(min_size=1))
@@ -41,8 +42,32 @@ def test_store_retrieve(client, msg):
     }))
 
     assert b'{"success": true}' in rv.data
+    assert rv.status_code == 200
 
     rv = client.get('/messages')
-    expected = json.dumps({"clientid": 100, "loglevel": 'info', "message": msg}).encode()
+    expected = json.dumps({
+        "clientid": 100,
+        "loglevel": 'info',
+        "message": msg
+    }).encode()
 
     assert expected in rv.data
+    assert rv.status_code == 200
+
+
+def test_bad_loglevel_post(client):
+    rv = client.post('/messages', data=dict({
+        'clientid': 100,
+        'loglevel': 'onfire',
+        'message': 'foobar'
+    }))
+
+    assert b'{"success": false, "error": "Bad log level"}' in rv.data
+    assert rv.status_code == 400
+
+
+def test_bad_loglevel_get(client):
+    rv = client.get('/messages?min_level=foobar')
+
+    assert b'{"success": false, "error": "Bad log level"}' in rv.data
+    assert rv.status_code == 400
