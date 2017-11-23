50f52eab9c0447470fdfd3476a414f7ccf1e4726
Steven Casagrande <scasagrande@galvant.ca>
2017-11-22 20:26:58 -0500

    Create the database schema

---
+++ logserv/schema.sql
@@ -0,0 +1,20 @@
+drop table if exists messages;
+drop table if exists loglevels;
+
+create table loglevels (
+ id integer primary key,
+ name text not null
+);
+
+insert into loglevels (id, name)
+values  (0, 'info'),
+        (1, 'warning'),
+        (2, 'error');
+
+create table messages (
+  id integer primary key autoincrement,
+  clientid integer not null,
+  loglevel text not null,
+  message text not null,
+  FOREIGN KEY(loglevel) REFERENCES loglevels(id)
+);
